531ae976c9d1e686bc88393ec1c993cc6b7c2aa9
Luiz Scheinkman <lscheinkman@numenta.com>
2016-05-11 13:53:30 -0700

    UNI-441: Fix bashrc

--- unicorn/vagrant-provision.sh
+++ unicorn/vagrant-provision.sh
@@ -32,6 +32,10 @@ rm /usr/local/share/man/man1/brew-cask.1
 sudo -u vagrant -i brew tap --repair
 sudo -u vagrant -i brew update
 
+# Initialize .bashrc with PATH
+sudo -u vagrant /usr/libexec/path_helper -s >> /Users/vagrant/.bashrc
+sudo -u vagrant ln -s .bashrc .bash_profile
+
 # Install cmake with homebrew
 sudo -u vagrant -i brew install cmake
 
@@ -39,8 +43,8 @@ sudo -u vagrant -i brew install cmake
 sudo -u vagrant -i brew uninstall node
 sudo -u vagrant -i brew install nvm
 sudo -u vagrant mkdir /Users/vagrant/.nvm
-sudo -u vagrant echo "export NVM_DIR=/Users/vagrant/.nvm" >> /Users/vagrant/.bash_profile
-sudo -u vagrant echo "source $(brew --prefix nvm)/nvm.sh" >> /Users/vagrant/.bash_profile
+sudo -u vagrant echo "export NVM_DIR=/Users/vagrant/.nvm" >> /Users/vagrant/.bashrc
+sudo -u vagrant echo "source $(brew --prefix nvm)/nvm.sh" >> /Users/vagrant/.bashrc
 
 # Switch nodejs to version specified in 'Vagrant' file
 sudo -u vagrant -i nvm install v$1
