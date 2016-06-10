79aa25cb0dd90ffa7f01f623eea4138c65ecd9d0
Andrew Malta <andrew.malta@yale.edu>
2016-06-10 16:33:36 -0700

    UNI-545 adds logging, awaiting the loggig pr to be merged

--- unicorn/app/main/FileService.js
+++ unicorn/app/main/FileService.js
@@ -26,6 +26,7 @@ import instantiator from 'json-schema-instantiator';
 import moment from 'moment';
 import path from 'path';
 import {Validator} from 'jsonschema';
+import log from ./Logger
 
 import config from './ConfigService';
 import {
@@ -70,6 +71,7 @@ const TIMESTAMP_HEADER_SUBSTRINGS = ['time', 'date'];
  */
 function isNA(entry) {
   if (typeof entry === 'undefined') {
+    log.error(new Error('Param "entry" of FileService.isNA() is not defined.'));
     return false;  // somewhat arbitrary decision to return false in this case
   }
   return NA_STRINGS.indexOf(entry.toString()
